60c6e2781adb45a8b40c420dbb0bcc9cf6f66210
s.umeda <syuhei19911212@gmail.com>
2024-05-14 22:42:09 +0900

    App classを作成

---
+++ js/App.js
@@ -0,0 +1,53 @@
+import NotesView from "./NotesView.js";
+import NotesAPI from "./NotesAPI.js";
+
+export default class App {
+  // constructor は初期化。App classs をインスタンス化した時に、自動的に以下処理が実行される。
+  constructor(root) {
+    this.notes = [];
+    this.activeNote = null;
+    this.view = new NotesView(root, this._handlers());
+
+    // 最初に呼ばれる
+    this._refreshNotes();
+  }
+
+  _refreshNotes() {
+    const notes = NotesAPI.getAllNotes();
+    console.log(notes);
+
+    this._setNotes(notes);
+
+    if (notes.length > 0) {
+      this._setActiveNote(notes[0]);
+    }
+  }
+
+  _setActiveNote(note) {
+    this.activeNote = note;
+    this.view.updateActiveNote(note);
+  }
+
+  _setNotes(notes) {
+    this.notes = notes;
+    this.view.updateNoteList(notes);
+  }
+
+  _handlers() {
+    return {
+      onNoteSelect: (noteId) => {
+        console.log(noteId + "のノートが選択されました");
+      },
+      onNoteAdd: () => {
+        console.log("ノートが追加されました");
+      },
+      onNoteEdit: (title, body) => {
+        console.log(title);
+        console.log(body);
+      },
+      onNoteDelete: (noteId) => {
+        console.log(noteId + "のノートが削除されました");
+      },
+    };
+  }
+}
--- js/main.js
+++ js/main.js
@@ -1,36 +1,7 @@
+import App from "./App.js";
 import NotesAPI from "./NotesAPI.js";
 import NotesView from "./NotesView.js";
 
-// NotesAPI.saveNote({
-//   id: 123456,
-//   title: "更新した2回目のメモです！",
-//   body: "プログラム作成中・・・・",
-// });
-
 // dom の中身を取得して app に渡す
-const app = document.getElementById("app");
-const view = new NotesView(app, {
-  onNoteSelect(id) {
-    console.log(id + "のノートが選択されました");
-  },
-  onNoteAdd() {
-    console.log("ノートが追加されました");
-  },
-  onNoteEdit(newTitle, newBody) {
-    console.log(newTitle);
-    console.log(newBody);
-  },
-  onNoteDelete(id) {
-    console.log(id + "のノートが削除されました");
-  },
-});
-
-console.log(NotesAPI.getAllNotes());
-
-const notes = NotesAPI.getAllNotes();
-
-// サイドバーにメモを全て表示
-view.updateNoteList(notes);
-
-// プレビューにメモ内容を表示する
-view.updateActiveNote(notes[0]);
+const root = document.getElementById("app");
+const app = new App(root);
